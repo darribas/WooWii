7a6c0dec7142372461d7b82aba41b342ce4c7dfb
Thomas de Graaff <t.de.graaff@vu.nl>
2014-10-01 12:39:07 +0200

    Revert "Changed assignment"
    This reverts commit 9842e22763a81fab0de82f0608f00c28e7a456ba.

--- Paper/Final/src/Example.R
+++
@@ -1,9 +0,0 @@
-nl <- read.csv("RR - Netherlands.csv")
-
-with_debt <- nl[!is.na(nl$Debt), ]
-head(with_debt, 3)
-
-ols <- lm('log(Debt) ~ log(GDP)', data=proc)
-summary(ols)
-
-plot(log(proc$GDP), log(proc$Debt))
